a9c8ef39a4339104617390e8d489ce564e8bef2c
revut <revutskyg@gmail.com>
2021-12-08 11:20:25 +0200

    sum

---
+++ src/components/App/App.jsx
@@ -0,0 +1,24 @@
+import Profile from "../Profile/Profile";
+import Statistics from "../Statistics/Statistics";
+import FriendList from "../FriendList/FriendList";
+import TransactionHistory from "../TransactionHistory/TransactionHistory";
+import user from "../Profile/user.json";
+import data from "../Statistics/data.json";
+import friends from "../FriendList/friends.json";
+import transactions from "../TransactionHistory/transactions.json";
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <>
+        <Profile user={user} />
+        <Statistics title="Upload stats" stats={data} />
+        <FriendList friends={friends} />
+        <TransactionHistory items={transactions} />
+      </>
+    </div>
+  );
+};
+
+export default App;
