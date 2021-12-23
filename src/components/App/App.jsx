af2c19584f67bb720f0a231237f1e4873f259aea
revut <revutskyg@gmail.com>
2021-12-23 09:38:51 +0200

    finish lang

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,20 +1,27 @@
 import Phonebook from "../Phonebook/Phonebook";
 import { Suspense } from "react";
+import Loader from "react-loader-spinner";
 
 import LangSwitcher from "../LangSwitcher/LangSwitcher.js";
 const App = () => {
   return (
     <div>
-      <Suspense fallback="loading...">
-        <div>
-          <LangSwitcher />
-        </div>
+      <Suspense
+        fallback={
+          <Loader
+            type="Bars"
+            color="#00BFFF"
+            height={80}
+            width={80}
+            timeout={3000}
+          />
+        }
+      >
+        <LangSwitcher />
       </Suspense>
-      <>
-        <Suspense fallback="loading...">
-          <Phonebook />
-        </Suspense>
-      </>
+      <Suspense fallback="">
+        <Phonebook />
+      </Suspense>
     </div>
   );
 };
